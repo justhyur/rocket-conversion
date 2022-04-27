971c948b1ae3a189bbe66d041f17d1d125483ac9
Riccardo Scrizzi <riccardoscrizzi@gmail.com>
2022-04-27 14:46:47 +0300

    init

---
+++ components/JumboTron.js
@@ -0,0 +1,27 @@
+
+export default function JumboTron() {
+  return (
+    <section id="jumbo-tron" className="section">
+        <div className="container">
+            <div className="centered-container">
+                <img src="/images/product-hunt-badge.svg"/>
+            </div>
+            <h1>How much money are you leaving on the table right now?</h1>
+            <p>Want to start or you already own a multiple figures online store? Get the most comprehensive step to step E-commerce Checklist that will <strong>skyrocket your sales.</strong></p>
+            <div className="centered-container">
+                <a className="button">
+                    <span>Get my Rocket Conversion List Now!</span>
+                </a>
+            </div>
+            <div className="video-container">
+                <iframe 
+                    src="https://www.youtube.com/embed/z-6T9lvTwJA?playlist=z-6T9lvTwJA&loop=1&autoplay=1&controls=0" 
+                    title="YouTube video player" 
+                    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                    allowfullscreen
+                ></iframe>
+            </div>
+        </div>
+    </section>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import { GlobalContext } from '/lib/context';
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <GlobalContext.Provider value={{}}>
+      {/* <Navbar /> */}
+      <Component {...pageProps} />
+    </GlobalContext.Provider>
+  ) 
 }
 
 export default MyApp
