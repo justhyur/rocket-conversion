fe2ad491d81362e0949ac0df581c068e43202001
Riccardo Scrizzi <riccardoscrizzi@gmail.com>
2022-06-07 15:20:32 +0400

    daff

--- components/FooterSection.js
+++ components/FooterSection.js
@@ -1,8 +1,8 @@
 export default function SatisfactionSection() {
   return (
     <section id="footer" className="section">
-      <a>Privacy Policy</a>
-      <a>Term and Conditions</a>
+      <a href="#">Privacy Policy</a>
+      <a href="#">Term and Conditions</a>
     </section>
   )
 }
