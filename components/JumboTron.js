b9bc299a1971a0780a88b896dc46db499d44dd35
Riccardo Scrizzi <riccardoscrizzi@gmail.com>
2022-06-17 19:44:49 +0400

    updated video

--- components/JumboTron.js
+++ components/JumboTron.js
@@ -29,8 +29,8 @@ export default function JumboTron() {
             </div>
             <div className="video-container">
                 <iframe 
-                    src="https://www.youtube.com/embed/z-6T9lvTwJA?playlist=z-6T9lvTwJA&loop=1&autoplay=1&controls=0" 
-                    title="YouTube video player" 
+                    src="https://www.youtube.com/embed/Wvva0Q6Ybb0?playlist=Wvva0Q6Ybb0&loop=1&autoplay=1&controls=0" 
+                    title="Rocket Profit Checklist Preview" 
                     frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
                     allowFullScreen
                 ></iframe>
