491f989392ea13b725ff19d8e452d48590e0a717
Riccardo Scrizzi <riccardoscrizzi@gmail.com>
2022-06-22 18:24:08 +0400

    pixel

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,21 @@
 import { GlobalContext } from '/lib/context';
+import Head from 'next/head';
+import FacebookPixel from '../components/FacebookPixel';
 import '../styles/globals.scss';
 import '../styles/flexmaster.scss'
 
 function MyApp({ Component, pageProps }) {
-  return(
-    <GlobalContext.Provider value={{}}>
-      {/* <Navbar /> */}
-      <Component {...pageProps} />
-    </GlobalContext.Provider>
+  const FB_PIXEL_ID = process.env.FB_PIXEL_ID;
+  console.log({FB_PIXEL_ID});
+  return(<>
+      <Head>
+        <FacebookPixel FB_PIXEL_ID={FB_PIXEL_ID}/>
+      </Head>
+      <GlobalContext.Provider value={{}}>
+        {/* <Navbar /> */}
+        <Component {...pageProps} />
+      </GlobalContext.Provider>
+    </>
   ) 
 }
 
